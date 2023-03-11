7d83085e2e4ab4f779b3449f489607fe2b2d4225
Manas Purohit <purohit.m@husky.neu.edu>
2023-03-11 13:25:35 -0500

    fetching train data and displaying on frontend

--- client/src/AppHeader.js
+++ client/src/AppHeader.js
@@ -1,20 +1,35 @@
 import { useQuery } from 'graphql-hooks'
 import './App.css';
 
-const HELLO_QUERY = `query HelloQuery {
-  hello
+const TRAINS_QUERY = `query TrainsQuery {
+  trains
 }`
 
 function AppHeader() {
   const { loading, error, data } = useQuery(HELLO_QUERY)
 
+  let content = null;
+
+  if (loading) {
+    content = <label>Loading</label>
+  }
+
+  if (error) {
+    content = <label>Error fetching Trains!</label>
+  }
+
+  if (data) {
+    content =
+    <ul>
+      {data.trains.map(train => <li>{train}</li>)}
+    </ul>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <label>
-          {loading && "Loading..."}
-          {error && "Error fetching Hello :("}
-          {data && data.hello}
+          {content}
         </label>
       </header>
     </div>
--- server/model.py
+++ server/model.py
@@ -1,7 +1,16 @@
 from ariadne import QueryType
+from nyct_gtfs import NYCTFeed
+from api_key import *
 
 query = QueryType()
+
+ace_feed = NYCTFeed("A", api_key=api_key)
+c_trains = ace_feed.filter_trips(line_id="C", travel_direction="N", underway=True)
 
 @query.field("hello")
 def resolve_hello(_, info):
   return "Hello from GraphQL!"
+
+@query.field("trains")
+def resolve_trains(_, info):
+  return list(map(str, c_trains))
