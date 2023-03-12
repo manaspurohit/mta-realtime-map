0f80c7d42bae99fc5eba993af4cd0df1966925c0
Manas Purohit <purohit.m@husky.neu.edu>
2023-03-11 21:57:49 -0500

    refactored server code to improve readability

---
+++ server/train.py
@@ -0,0 +1,7 @@
+class Train:
+  def __init__(self, id, top, left, status, direction):
+    self.id = id
+    self.top = top
+    self.left = left
+    self.status = status
+    self.direction = direction
